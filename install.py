d3038899c7ffdcdee15fb98bfc1e7dc058c2f910
leleneme <joaolemss@gmail.com>
2024-09-04 08:16:57 -0300

    meta: add alacritty and fix some problems with install.py

--- install.py
+++ install.py
@@ -1,20 +1,30 @@
 #!/usr/bin/env python
 
 import subprocess
+import os
 
 def get_link_targets() -> list[tuple[str, str]]:
-    return [("./emacs/", "~/.config")]
+    return [
+        ("emacs", "~/.config/"),
+        ("alacritty", "~/.config"),
+    ]
 
 def symlink(source: str, target: str) -> tuple[bool, str]:
-    sp = subprocess.run(['ln', '-s', source, target], capture_output=True)
-    return (sp.returncode == 0, sp.stdout.decode('utf-8'))
+    sp = subprocess.run(["ln", "-sf", source, target], capture_output=True)
+    return (sp.returncode == 0, sp.stderr.decode("utf-8"))
+
+def abspath(path: str) -> str:
+    return os.path.abspath(os.path.expanduser(path))
 
 def main():
     targets = get_link_targets()
-    for (source, target) in targets:
+    targets = map(lambda t: map(abspath, t), targets)
+
+    for source, target in targets:
+        print(f"Linking {source} to {target}")
         (ok, err) = symlink(source, target)
         if not ok:
-            print(f'Symlink failed: {err}')
+            print(f"Symlink failed: {err}")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
