1de89ba3053478f1df72abc13599ced25cda7804
leleneme <joaolemss@gmail.com>
2024-09-15 05:09:43 -0300

    bash: add SSH_AUTH_SOCK env variable

--- bash/env.bash
+++ bash/env.bash
@@ -5,6 +5,9 @@ export EDITOR=micro
 # disable dotnet telemetry
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
+# ssh-agent socket
+export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
+
 PATH=$PATH:$HOME/.dotnet/tools/
 PATH=$PATH:$HOME/go/bin
 PATH=$PATH:$HOME/.cargo/bin
